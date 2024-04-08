0a5262c6b136a223d75972fa97cff9eaf5ba478e
Nick_Join <nickjoin79@gmail.com>
2024-04-08 20:15:59 +0400

    FSD - Feature-Sliced Design

---
+++ src/app/appEntry.tsx
@@ -0,0 +1,4 @@
+import createRoot from 'react-dom'
+import BaseLayout from '@/app/layouts/BaseLayout'
+
+createRoot.render(<BaseLayout/>, document.getElementById('root'))
---
+++ src/app/layouts/BaseLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Header} from '@/widgets/header'
+import {Footer} from '@/widgets/footer'
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            Трахать негра
+            <Footer/>
+        </div>
+    )
+}
+
+export default App
