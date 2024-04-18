4c119c1044a324abfb968d757a5790d04229b514
Nick_Join <nickjoin79@gmail.com>
2024-04-18 11:53:15 +0400

    start of work

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Header} from '@/widgets/header'
 import {Footer} from '@/widgets/footer'
+import {Outlet} from 'react-router-dom'
 
 const App = () => {
     return (
         <div>
             <Header/>
-            Трахать негра
+            <Outlet/>
             <Footer/>
         </div>
     )
